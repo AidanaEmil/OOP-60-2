f9f664833593c0896f798c8706c392e3a134d75c
AidanaEmil <ajdanaemilbekova364@gmail.com>
2025-11-10 20:39:51 +0600

    First commit

---
+++ lesson_1.py
@@ -0,0 +1,11 @@
+class Hero:
+    def __init__(self, nick_name, lvl , hp):
+        self.nick_name = nick_name
+        self.lvl = lvl
+        self.hp = hp
+
+kirito = Hero('Kirito', 100, 1000)
+asuna = Hero('Asuna', 100, 1000)
+
+print(kirito.lvl)
+print(kirito.hp)
