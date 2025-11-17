c342a85849e427e3ec866d713fc81f5224d90a25
AidanaEmil <ajdanaemilbekova364@gmail.com>
2025-11-17 22:02:59 +0600

    Обновила код Ð

--- homework_1.py
+++ homework_1.py
@@ -1,23 +1,22 @@
-class Person:
-    def __init__(self, name, birth_date, occupation, higher_education):
-        self.name = name
-        self.birth_date = birth_date
-        self.occupation = occupation
-        self.higher_education = higher_education
+class Car:
+  def __init__(self, brand, model, speed):
+    self.brand=brand
+    self.model=model
+    self.speed=speed
 
-    def introduce(self):
-        education = "есть высшее образование" if self.higher_education else "нет высшего образования"
-        print(f"Привет! Меня зовут {self.name}. Я {self.occupation}, родился(лась) {self.birth_date}, и у меня {education}.")
+  def description(self):
+      return f"Машина: {self.brand} {self.model}, скорость: {self.speed} км/ч"
 
-person1 = Person("Айдана", "12 марта 2004", "дизайнер", True)
-person2 = Person("Айдина", "25 июля 2005", "программист", False)
-person3 = Person("Адинай", "9 января 2003", "фотограф", True)
+  def accelerate(self, increase):
+     self.speed+=increase
+     return f"Скорость увеличена до {self.speed} км/ч"
 
-print(person1.name, person1.birth_date, person1.occupation, person1.higher_education)
-print(person2.name, person2.birth_date, person2.occupation, person2.higher_education)
-print(person3.name, person3.birth_date, person3.occupation, person3.higher_education)
-print()
-person1.introduce()
-person2.introduce()
-person3.introduce()
+car1=Car("Toyota","Corolla",120)
+car2=Car("BMW","X5",150)
+
+print(car1.description())
+print(car1.accelerate(20))
+
+print(car2.description())
+print(car2.accelerate(30))
 
